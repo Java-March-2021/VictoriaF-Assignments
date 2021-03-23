f29f49abc03216c3e8ce9d642d192011edb50ae3
vicfuen94 <fuentesvictoria923@gmail.com>
2021-03-22 21:57:05 -0500

    PythagoreanTheorem

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTheoremTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTheoremTest {
+    public static void main(String[] args){
+        PythagoreanTheorem pt = new PythagoreanTheorem();
+        System.out.println(pt.calculateHypotenuse(6,36));
+    }
+}
