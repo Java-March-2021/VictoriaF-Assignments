fbbb16427bbf279648fd977e6b62f291cb0d9f00
vicfuen94 <fuentesvictoria923@gmail.com>
2021-03-22 21:17:21 -0500

    FirstJava

---
+++ 01-java-fundamentals/01-first-java-program/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstJavaProgram {
+    public static void main (String[] args) {
+        System.out.println("My name is Victoria Fuentes.");
+        System.out.println("I am 26 years old");
+        System.out.println("and I live in Carrollton, Texas.");
+    }
+}
