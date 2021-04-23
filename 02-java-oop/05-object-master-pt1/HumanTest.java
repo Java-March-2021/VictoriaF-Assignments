31b059afd1ae234f67a3982bcc344c21f50855e8
vicfuen94 <fuentesvictoria923@gmail.com>
2021-04-22 23:06:06 -0500

    Objest Master

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,11 @@
+public class HumanTest{
+    public status void main(String[] args) {
+        Human Victoria = new Human();
+        Human Marco = new Human(100,10,5,2);
+
+        Marco.attack(Victoria);
+        System.out.println(Victoria.health);
+
+    }
+    
+}
