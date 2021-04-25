3ec4713220bfc68aebd6c3ae7508f82102e72b1f
vicfuen94 <fuentesvictoria923@gmail.com>
2021-04-25 11:01:43 -0500

    survey

---
+++ 03-java-spring/01-spring-fundamentals/07-dojo-survey/DojoSurvey/src/test/java/com/victoria/dojosurvey/DojoSurveyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.victoria.dojosurvey;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DojoSurveyApplicationTests {
+	@Test
+	public void contextLoads() {
+		
+	}
+	
+
+}
